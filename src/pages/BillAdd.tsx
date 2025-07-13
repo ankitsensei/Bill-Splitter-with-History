eaa9100d6b9816daf5ee1e2dd8959ea8dee62e5d
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-14 01:03:12 +0530

    navigation

--- src/pages/BillAdd.tsx
+++ src/pages/BillAdd.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+
 
 
 const BillAdd = () => {
@@ -9,13 +11,13 @@ const BillAdd = () => {
     console.log(watch("example"));
 
     return (
-        <div className="my-6 px-4 h-screen flex flex-col justify-between gap-10 w-full sm:w-[450px] md:w-[450px] lg:w-[450px]">
+        <div className="my-6 px-4 h-screen flex flex-col justify-between w-full sm:w-[450px] md:w-[450px] lg:w-[450px]">
+            <Link to="/"><button className="text-start underline hover:cursor-grab">back</button></Link>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full mt-10">
                 <div>
                     <label htmlFor="">Money spent on?</label>
                     <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Street food" />
                 </div>
-
                 <div className="flex gap-2">
                     <div>
                         <label htmlFor="">How much?</label>
@@ -30,7 +32,6 @@ const BillAdd = () => {
                         <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="3" />
                     </div>
                 </div>
-
 
                 <div>
                     <label htmlFor="">Name of Persons</label>
