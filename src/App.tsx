f2662aec541c0f37873c65ef29f61dae4e75c14c
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-13 11:20:04 +0530

    alright, react router working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import Home from "./pages/Home"
 
 const App = () => {
+  // const root = document.getElementById("root");
+
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+
+const home = () => {
+  return (
+    <div className='text-8xl font-semibold'>home</div>
+  )
+}
+
+export default home
