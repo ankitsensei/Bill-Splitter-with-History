dd02dfe7c52f64027381f73057383db7c33e0808
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-14 00:47:59 +0530

    Add btn working routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import Btn from "../comp/Btn"
+import { Link } from "react-router";
 
 const home = () => {
-  
+
   return (
     <div className="mt-20 px-4 flex flex-col gap-10 w-full lg:w-1/4">
       <div className="mt-20">
-        <Btn title="+"/>
+        <Link to="/billadd"><Btn title="+" /></Link>
       </div>
     </div>
   )
