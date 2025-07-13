c4a23a03bc44931a7cfcc7a238976e53298a764c
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-14 00:26:01 +0530

    UI changed

--- src/pages/BillAdd.tsx
+++ src/pages/BillAdd.tsx
@@ -9,8 +9,8 @@ const BillAdd = () => {
     console.log(watch("example"));
 
     return (
-        <div className="my-6 px-4 h-screen flex flex-col justify-between gap-10 w-full lg:w-1/4">
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 w-full mt-10">
+        <div className="my-6 px-4 h-screen flex flex-col justify-between gap-10 w-full sm:w-[450px] md:w-[450px] lg:w-[450px]">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full mt-10">
                 <div>
                     <label htmlFor="">Money spent on?</label>
                     <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Street food" />
@@ -25,12 +25,13 @@ const BillAdd = () => {
                         <label htmlFor="">When?</label>
                         <input type="date" defaultValue={today} {...register("example")} className="border-zinc-600  border-2 p-2 rounded w-full outline-none" />
                     </div>
+                    <div>
+                        <label htmlFor="">No. of ppl?</label>
+                        <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="3" />
+                    </div>
                 </div>
 
-                <div>
-                    <label htmlFor="">No. of person?</label>
-                    <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="3" />
-                </div>
+
                 <div>
                     <label htmlFor="">Name of Persons</label>
 
