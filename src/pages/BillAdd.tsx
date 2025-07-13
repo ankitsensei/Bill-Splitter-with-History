baddfbb7772ba891c203738ce86350781de15c78
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-13 16:58:46 +0530

    added nno. of ppl

--- src/pages/BillAdd.tsx
+++ src/pages/BillAdd.tsx
@@ -9,12 +9,28 @@ const BillAdd = () => {
     console.log(watch("example"));
 
     return (
-        <div className="mt-20 px-4 flex flex-col gap-10 w-full lg:w-1/4">
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 w-full">
-                <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Money spent on?" />
+        <div className="my-6 px-4 h-screen flex flex-col justify-between gap-10 w-full lg:w-1/4">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 w-full mt-10">
+                <div>
+                    <label htmlFor="">Money spent on?</label>
+                    <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Money spent on?" />
+                </div>
+
                 <div className="flex gap-2">
-                    <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="How much?" />
-                    <input type="date" defaultValue={today} {...register("example")} className="border-zinc-600  border-2 p-2 rounded w-full outline-none" placeholder="When?" />
+                    <div>
+                        <label htmlFor="">How much?</label>
+                        <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="How much?" />
+                    </div>
+                    <div>
+                        <label htmlFor="">When?</label>
+                        <input type="date" defaultValue={today} {...register("example")} className="border-zinc-600  border-2 p-2 rounded w-full outline-none" placeholder="When?" />
+                    </div>
+                </div>
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <label htmlFor="">Number of people?</label>
+                        <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-50 outline-none" placeholder="Number of people?" />
+                    </div>
                 </div>
             </form>
             {errors.exampleRequired && <span>This field is required</span>}
