b75f3144cc8b5cad72fd177998711945b85dbcac
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-14 13:23:23 +0530

    Fix: Added an array where name of persons will be stored and with ,map will be accessed as selection options

--- src/pages/BillAdd.tsx
+++ src/pages/BillAdd.tsx
@@ -1,14 +1,30 @@
-import { useForm } from "react-hook-form";
-import { Link } from "react-router";
+import { useState } from 'react'
 
+import { Link } from "react-router";
+import { useForm, } from "react-hook-form";
+// import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    spentOn: string,
+    howMuch: number,
+    when: string,
+    noOfPpl: number,
+    nameOfPersons: Array<string>,
+    whoPaid: string,
+    noOfPerson: Array<string>,
+};
 
 
 const BillAdd = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
     const today = new Date().toISOString().split('T')[0];
 
-    console.log(watch("example"));
+    const [noOfPerson, setNoOfPerson] = useState(["Ankit", "Shyam", "Rohit"]);
+
+    console.log(watch("spentOn"));
+
 
     return (
         <div className="my-6 px-4 h-screen flex flex-col w-full sm:w-[450px] md:w-[450px] lg:w-[450px]">
@@ -17,41 +33,45 @@ const BillAdd = () => {
                 <div className="flex flex-col gap-4  w-full mt-15">
                     <div>
                         <label htmlFor="">Money spent on?</label>
-                        <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Street food" />
+                        <input type="text" {...register("spentOn", { required: true })} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Street food" />
+                        {errors.spentOn && <span>This field is required</span>}
                     </div>
                     <div className="flex gap-2">
                         <div>
                             <label htmlFor="">How much?</label>
-                            <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="300" />
+                            <input type="number" {...register("howMuch", { required: true })} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="300" />
+                            {errors.howMuch && <span>This field is required</span>}
                         </div>
                         <div>
                             <label htmlFor="">When?</label>
-                            <input type="date" defaultValue={today} {...register("example")} className="border-zinc-600  border-2 p-2 rounded w-full outline-none" />
+                            <input type="date" defaultValue={today} {...register("when", { required: true })} className="border-zinc-600  border-2 p-2 rounded w-full outline-none" />
+                            {errors.when && <span>This field is required</span>}
                         </div>
                         <div>
                             <label htmlFor="">No. of ppl?</label>
-                            <input type="number" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="3" />
+                            <input type="number" {...register("noOfPpl", { required: true })} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="3" />
+                            {errors.noOfPpl && <span>This field is required</span>}
                         </div>
                     </div>
 
                     <div>
                         <label htmlFor="">Name of Persons</label>
 
-                        <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Ankit" />
-                        <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Shyam" />
-                        <input type="text" {...register("example")} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Rohit" />
+                        <input type="text" {...register("nameOfPersons", { required: true })} className="border-zinc-600 border-2 p-2 rounded w-full outline-none" placeholder="Ankit, Shyam, Rohit" />
                     </div>
                     <div>
                         <label htmlFor="">Who paid?</label>
-                        <select id="whoPaid" {...register("whoPaid")} className="border-zinc-600 bg-black text-white border-2 p-2 rounded w-full outline-none"
+                        <select id="whoPaid" {...register("whoPaid", { required: true })} className="border-zinc-600 bg-black text-white border-2 p-2 rounded w-full outline-none"
                         >
-                            <option value="Ankit">Ankit</option>
-                            <option value="Shyam">Shyam</option>
-                            <option value="Rohit">Rohit</option>
+                            {
+                                noOfPerson.map((name, index) => (
+                                    <option key={index} value={name}>{name}</option>
+                                ))
+                            }
                         </select>
+                        {errors.whoPaid && <span>This field is required</span>}
                     </div>
                 </div>
-                {errors.exampleRequired && <span>This field is required</span>}
                 <p className="text-center">Each person will pay <span className="font-semibold underline">Rs.100</span> to <span className="font-semibold underline">Ankit</span>.</p>
                 <input type="submit" className="px-20 py-2 bg-white text-black rounded" />
             </form>
