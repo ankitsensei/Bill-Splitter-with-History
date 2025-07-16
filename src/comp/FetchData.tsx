a74fa8893593287523993343403edd7d9c3eb7b1
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-16 14:01:38 +0530

    UI enhanced

--- src/comp/FetchData.tsx
+++ src/comp/FetchData.tsx
@@ -40,8 +40,8 @@ const FetchData = () => {
             {
                 data.map((item) => (
                     <ul className='border-zinc-400 border-1 p-2 rounded-2xl'>
-                        <div className='text-zinc-400'>
-                            <div className='bg-amber-500 text-white py-2 rounded-t-xl'>
+                        <div className='text-zinc-400 flex flex-col gap-1'>
+                            <div className='bg-purple-800 text-white py-2 rounded-t-xl'>
                                 <li className='text-4xl text-center'>-₹{item.howMuch}</li>
                                 <div className='flex justify-between px-2'>
                                     <li><span>{item.moneySpentOn}</span></li>
@@ -49,12 +49,12 @@ const FetchData = () => {
                                 </div>
 
                             </div>
-                            <div className='flex gap-2'>
-                                <li className='text-white'><span className='text-white'>{item.noOfPpl}</span> people: </li>
+                            <div className='flex gap-2 justify-start'>
+                                <li className='text-white w-20'><span className='text-white'>{item.noOfPpl}</span> people: </li>
                                 <li><span>{item.nameOfPpl}</span></li>
 
                             </div>
-                            <li>Who paid: <span>{item.whoPaid}</span></li>
+                            <li className=''>Who paid: <span className='px-2 bg-amber-400 text-black rounded-lg'>{item.whoPaid}</span></li>
                             <li>Each person will pay <span className='px-2 bg-green-400 text-black rounded-lg'>Rs.{item.individualBill}</span> to <span className='px-2 bg-amber-400 text-black rounded-lg'>{item.whoPaid}</span>.</li>
                         </div>
                     </ul>
