b084a41dd3207116d9ab882b619956ef45573d63
ankitsensei <main.ankitbhagat@gmail.com>
2025-07-16 13:53:07 +0530

    UI updated

--- src/comp/FetchData.tsx
+++ src/comp/FetchData.tsx
@@ -6,7 +6,7 @@ interface BillHistoryItem {
     howMuch: number;
     when: string;
     noOfPpl: number;
-    nameOfPersons: string[];
+    nameOfPpl: string[];
     whoPaid: string;
     noOfPerson: number;
     individualBill: number;
@@ -35,26 +35,33 @@ const FetchData = () => {
 
     if (loading) return <p>Loading...</p>;
     return (
-        <div>
-            <h2 className='text-4xl'>Fetched Data:</h2>
+        <div className='w-lvw px-4'>
+            <h2 className='text-4xl pb-2'>Expense:</h2>
             {
                 data.map((item) => (
-                    <ul className='border-zinc-400 border-1 p-2 w-full rounded-md'>
-                        <li>Spent on: {item.moneySpentOn}</li>
-                        <li>How much: {item.howMuch}</li>
-                        <li>No. of ppl: {item.noOfPerson}</li>
-                        <li>Name of Ppl: {item.nameOfPersons}</li>
-                        <li>Who paid: {item.whoPaid}</li>
-                        <li>Each person will pay Rs.{item.individualBill} to {item.whoPaid}.</li>
+                    <ul className='border-zinc-400 border-1 p-2 rounded-2xl'>
+                        <div className='text-zinc-400'>
+                            <div className='bg-amber-500 text-white py-2 rounded-t-xl'>
+                                <li className='text-4xl text-center'>-₹{item.howMuch}</li>
+                                <div className='flex justify-between px-2'>
+                                    <li><span>{item.moneySpentOn}</span></li>
+                                    <li className='text-end'>{item.when}</li>
+                                </div>
+
+                            </div>
+                            <div className='flex gap-2'>
+                                <li className='text-white'><span className='text-white'>{item.noOfPpl}</span> people: </li>
+                                <li><span>{item.nameOfPpl}</span></li>
+
+                            </div>
+                            <li>Who paid: <span>{item.whoPaid}</span></li>
+                            <li>Each person will pay <span className='px-2 bg-green-400 text-black rounded-lg'>Rs.{item.individualBill}</span> to <span className='px-2 bg-amber-400 text-black rounded-lg'>{item.whoPaid}</span>.</li>
+                        </div>
                     </ul>
                 ))
             }
-
         </div>
     )
 }
 
 export default FetchData
-
-
-
