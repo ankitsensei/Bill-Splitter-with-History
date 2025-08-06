8db4f13299438333620b23338053890aeef3f441
ankitsensei <main.ankitbhagat@gmail.com>
2025-08-06 09:32:21 +0530

    new feature: added SettledFetchData page

---
+++ src/comp/SettledFetchData.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { supabase } from "../supabaseClient";
+
+interface BillHistoryItem {
+    moneySpentOn: string;
+    howMuch: number;
+    when: string;
+    noOfPpl: number;
+    nameOfPpl: string[];
+    whoPaid: string;
+    noOfPerson: number;
+    individualBill: number;
+}
+
+
+const SettledFetchData = () => {
+    const [data, setData] = useState<BillHistoryItem[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchFromSupabase = async () => {
+            const { data, error } = await supabase.from('settled').select('*');
+
+            if (error) {
+                console.log("Error fetching: ", error);
+            } else {
+                setData(data);
+            }
+            setLoading(false);
+        }
+        fetchFromSupabase();
+    }, [])
+
+    console.log(data);
+
+    if (loading) return <p>Loading...</p>;
+    return (
+        <div className='w-lvw sm:w-[450px] md:w-[450px] lg:w-[450px] px-4 pb-20 flex flex-col gap-2'>
+            <h2 className='text-2xl  pb-2 text-zinc-400'>Settled</h2>
+            {
+                data.map((item) => (
+                    <ul className='border-zinc-800 border-1 p-2 rounded-2xl'>
+                        <div className='text-zinc-400 flex flex-col gap-1'>
+                            <div className='bg-purple-800 text-white py-2 rounded-t-xl'>
+                                <li className='text-4xl text-center'>-₹{item.howMuch}</li>
+                                <div className='flex justify-between px-2'>
+                                    <li><span>{item.moneySpentOn}</span></li>
+                                    <li className='text-end text-zinc-100'>{item.when}</li>
+                                </div>
+
+                            </div>
+                            <div className='flex gap-2 justify-start'>
+                                <li className='text-white w-20'><span className='text-white'>{item.noOfPpl}</span> people: </li>
+                                <li><span>{item.nameOfPpl}</span></li>
+
+                            </div>
+                            <li className=''>Who paid: <span className='px-2 bg-amber-400 text-black rounded-lg'>{item.whoPaid}</span></li>
+                            <li>Each person will pay <span className='px-2 bg-green-400 text-black rounded-lg'>Rs.{item.individualBill}</span> to <span className='px-2 bg-amber-400 text-black rounded-lg'>{item.whoPaid}</span>.</li>
+                        </div>
+                    </ul>
+                ))
+            }
+        </div>
+    )
+}
+
+export default SettledFetchData
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import BillHistory from "./BillHistory";
 import { Link } from "react-router";
 import Btn from "../comp/Btn"
+import SettledFetchData from "../comp/SettledFetchData";
 const home = () => {
 
   return (
     <div className="mt-10 flex h-full flex-col justify-between">
       <div className="">
+        <SettledFetchData />
         <BillHistory />
       </div>
       <Link to="/billadd" className="w-full px-4 absolute bottom-4 sticky"><Btn title="+" /></Link>
